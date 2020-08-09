40fc8d17b2cb19fc93625b679a28e30ea6580623
Pushpanshu Tripathi <tripathipushpanshu0501@gmail.com>
2020-08-09 13:58:37 +0530

    images stitched with proper orientation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AlignImage
+To Run the project:<br />
+  `python3 stitch.py`<br />
+  Number of images input:<br />
+  Name of images:<br />
+  
+A window of Resulted stitched image is created.
---
+++ stitch.py
@@ -0,0 +1,39 @@
+from panorama import Panaroma
+import imutils
+import cv2
+
+print("Enter the number of images you want to concantenate:")
+no_of_images = int(input())
+print("Enter the image name in order of left to right in way of concantenation:")
+filename = []
+
+for i in range(no_of_images):
+    print("Enter the %d image:" %(i+1))
+    filename.append(input())
+
+images = []
+
+for i in range(no_of_images):
+    images.append(cv2.imread(filename[i]))
+
+for i in range(no_of_images):
+    images[i] = imutils.resize(images[i], width=400)
+for i in range(no_of_images):
+    images[i] = imutils.resize(images[i], height=400)
+
+
+panaroma = Panaroma()
+if no_of_images==2:
+    (result, matched_points) = panaroma.image_stitch([images[0], images[1]], match_status=True)
+else:
+    (result, matched_points) = panaroma.image_stitch([images[no_of_images-2], images[no_of_images-1]], match_status=True)
+    for i in range(no_of_images - 2):
+        (result, matched_points) = panaroma.image_stitch([images[no_of_images-i-3],result], match_status=True)
+
+
+#cv2.imshow("Keypoint Matches", matched_points)
+cv2.imshow("StitchedImmage", result)
+cv2.imwrite("Result.jpg",result)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
